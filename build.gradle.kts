a83a65b093a70906bc33f68e027d449bf416287d
Faceless <146445263+TheRealFaceless@users.noreply.github.com>
2024-07-27 23:54:54 +0200

    Changed to gradle

---
+++ build.gradle.kts
@@ -0,0 +1,54 @@
+plugins {
+    id("java")
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+}
+
+group = "net.minervamc"
+version = "0.1.0"
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+}
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://oss.sonatype.org/content/groups/public/")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.18.2-R0.1-SNAPSHOT")
+}
+
+tasks.assemble {
+    dependsOn(tasks.shadowJar)
+}
+
+tasks.shadowJar {
+    archiveClassifier.set("")
+    val additionalPaths = listOf("path/to/first/additional/directory", "path/to/second/additional/directory")
+    val default = "${layout.buildDirectory.get().asFile}/libs"
+
+    var pluginDir = file("null")
+    try {
+        pluginDir = file("D:/Servers/Minerva/plugins")
+    }catch (_: InvalidUserDataException) {}
+
+    if (pluginDir.exists()) destinationDirectory.set(pluginDir)
+    else destinationDirectory.set(file(default))
+
+    doLast {
+        additionalPaths.forEach { additionalPath ->
+            if (file(additionalPath).exists()) {
+                copy {
+                    from(archiveFile)
+                    into(additionalPath)
+                }
+            }
+        }
+    }
+}
+
+tasks.assemble {
+    dependsOn(tasks.shadowJar)
+}
