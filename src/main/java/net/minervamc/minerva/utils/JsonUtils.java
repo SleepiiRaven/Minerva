f8b28253b708a6203df5baa22b15ff242ae62b68
Faceless <146445263+TheRealFaceless@users.noreply.github.com>
2024-07-29 13:45:14 +0200

    Updated gitignore

--- src/main/java/net/minervamc/minerva/utils/JsonUtils.java
+++ src/main/java/net/minervamc/minerva/utils/JsonUtils.java
@@ -5,7 +5,7 @@ import com.google.gson.GsonBuilder;
 import net.minervamc.minerva.PlayerStats;
 
 public class JsonUtils {
-    private static GsonBuilder BUILDER = new GsonBuilder().registerTypeAdapter(PlayerStats.class, new PlayerStatsAdapter())
+    private static final GsonBuilder BUILDER = new GsonBuilder().registerTypeAdapter(PlayerStats.class, new PlayerStatsAdapter())
             .disableHtmlEscaping();
     public static Gson GSON;
 
