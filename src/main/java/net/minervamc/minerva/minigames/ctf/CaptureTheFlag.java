f73146c45b5ffebb2befd1b88ae36ba76b2e4c09
SleepiiRaven <104394825+SleepiiRaven@users.noreply.github.com>
2024-09-22 11:56:54 -0700

    nyet

--- src/main/java/net/minervamc/minerva/minigames/ctf/CaptureTheFlag.java
+++ src/main/java/net/minervamc/minerva/minigames/ctf/CaptureTheFlag.java
@@ -1,16 +1,27 @@
 package net.minervamc.minerva.minigames.ctf;
 
+import com.mysql.cj.jdbc.MysqlConnectionPoolDataSource;
+import com.mysql.cj.jdbc.MysqlDataSource;
 import java.util.List;
+import net.minervamc.minerva.Minerva;
 import net.minervamc.minerva.minigames.Minigame;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 public class CaptureTheFlag extends Minigame {
 
     @Override
     public void start(List<Player> players) {
+
+        MysqlDataSource dataSource = new MysqlConnectionPoolDataSource();
+        dataSource.setServerName(database.getHost());
+        dataSource.setPortNumber(database.getPort());
+        dataSource.setDatabaseName(database.getDatabase());
+        dataSource.setUser(database.getUser());
+        dataSource.setPassword(database.getPassword());
+        dataSource.getConnection();
         for (Player player : players) {
             // Save inventories
-            // 
         }
     }
 
